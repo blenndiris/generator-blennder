3b9fdac832ffc52ad5a2f7664576984ec815a09b
Seth Whitaker <seth@blennd.com>
2019-02-07 23:34:07 -0700

    Created Project Generator

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,37 +2,236 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const _ = require('lodash');
 
 module.exports = class extends Generator {
-  prompting() {
+
+
+  async prompting() {
     // Have Yeoman greet the user.
     this.log(
-      yosay(`Welcome to the best ${chalk.red('generator-blennder')} generator!`)
+      yosay(`Welcome to the ${chalk.red('generator-blennder')} project generator!`)
     );
 
-    const prompts = [
+    const prompts = await this.prompt([
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
-    ];
-
-    return this.prompt(prompts).then(props => {
+        type    : 'input',
+        name    : 'client_name',
+        message : 'Your Client Name',
+        default : 'Client' // Default to current folder name
+      },
+      {
+        type    : 'input',
+        name    : 'url',
+        message : 'What is your site URL?',
+        default : _.kebabCase(this.appname)+'.local'
+      },
+      {
+        type    : 'list',
+        name    : 'type',
+        message : 'Which Stack are you using?',
+        choices : ['MAMP', 'Valet', 'Flywheel']
+      },
+      {
+        type    : 'input',
+        name    : 'dbname',
+        message : 'What is your Database Table Name?',
+        default : _.kebabCase(this.appname),
+        when : function(answers){
+          if(answers.type == 'MAMP')
+          return true;
+        }
+      },
+      {
+        type    : 'input',
+        name    : 'dbuser',
+        message : 'What is your Database User Name?',
+        default : 'root',
+        when : function(answers){
+          if(answers.type == 'MAMP')
+          return true;
+        }
+      },
+      {
+        type    : 'input',
+        name    : 'dbpass',
+        message : 'What is your Database Password?',
+        default : 'root',
+        when : function(answers){
+          if(answers.type == 'MAMP')
+          return true;
+        }
+      },
+      {
+        type    : 'input',
+        name    : 'admin_user',
+        message : 'What is your Wordpress Admin Username?',
+        default : 'Ninjas',
+        when : function(answers){
+          if(answers.type == 'MAMP')
+          return true;
+        }
+      },
+      {
+        type    : 'confirm',
+        name    : 'autogen_pass',
+        message : 'Auto Generate Password',
+        default : true,
+        when : function(answers){
+          if(answers.type == 'MAMP')
+          return true;
+        }
+      },
+      {
+        type    : 'password',
+        name    : 'admin_password',
+        message : 'What is your Wordpress Admin Password?',
+        default : 'password',
+        when    : function(answers){
+          if(answers.autogen_pass ==  false)
+          return true;
+        }
+      },
+      {
+        type    : 'input',
+        name    : 'admin_email',
+        message : 'What is your Wordpress Admin Email?',
+        default : 'seth@blennd.com',
+        when : function(answers){
+          if(answers.type == 'MAMP')
+          return true;
+        }
+      },
+      {
+        type    : 'input',
+        name    : 'blogdescription',
+        message : 'Please Enter a Wordpress Site Description:',
+        default : 'Some New Site Description',
+        when : function(answers){
+          if(answers.type == 'MAMP')
+          return true;
+        }
+      },
+    ]).then(props => {
       // To access props later use this.props.someAnswer;
       this.props = props;
-    });
+    });;
+  }
+
+  configuring() {
+    this.props.theme_key = _.kebabCase(this.appname);
+    this.props.theme_title = _.startCase(this.appname);
+  }
+
+  blennder() {
+    this.log('Begin Installing Blennder Theme...');
+    this.spawnCommandSync('git', ['clone', '--depth=1', '--quiet', '--progress', 'git@github.com:blenndiris/blennder.git', '.']);
+    this.spawnCommandSync('git', ['clone', '--depth=1', '--quiet', '--progress', 'git@github.com:blenndiris/blennd-plugins.git', './wp-content/plugins/']);
+    this.spawnCommandSync('mv', ['./wp-content/themes/blennder', './wp-content/themes/'+this.props.theme_key]);
+    this.spawnCommandSync('rm', ['-rf', './wp-content/themes/'+this.props.theme_key+'/app/vendor/']);
+    this.spawnCommand('rm', ['-rf', '.git/']);
+    this.spawnCommand('rm', ['-rf', './wp-content/plugins/.git']);
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+    this.log('Begin Updating Files...');
+
+    this.fs.copyTpl(
+      this.templatePath( 'style.css' ),
+      this.destinationPath( './wp-content/themes/'+this.props.theme_key+'/style.css' ),
+      {
+        theme : this.props.theme_title
+      }
+    );
+    this.fs.copyTpl(
+      this.templatePath( '_variables.scss' ),
+      this.destinationPath( './wp-content/themes/'+this.props.theme_key+'/assets/scss/_variables.scss' ),
+      {
+        theme : this.props.theme_key
+      }
+    );
+    this.fs.copyTpl(
+      this.templatePath( '.gitignore' ),
+      this.destinationPath( '.gitignore' ),
+      {
+        theme : this.props.theme_key
+      }
+    );
+    this.fs.copyTpl(
+      this.templatePath( 'composer.json' ),
+      this.destinationPath( 'composer.json' ),
+      {
+        theme : this.props.theme_key
+      }
+    );
+    this.fs.copyTpl(
+      this.templatePath( 'package.json' ),
+      this.destinationPath( 'package.json' ),
+      {
+        theme : this.props.theme_key
+      }
+    );
+    this.fs.copyTpl(
+      this.templatePath( 'webpack.mix.js' ),
+      this.destinationPath( 'webpack.mix.js' ),
+      {
+        theme : this.props.theme_key,
+        url : this.props.url
+      }
     );
   }
 
   install() {
-    this.installDependencies();
+    this.log("Begin Installing Composer Pacakges...");
+    this.spawnCommand('composer', ['install']);
+    this.log("Begin Installing NPM Pacakges...");
+    this.spawnCommandSync('npm', ['install', '--silent']);
   }
+
+  end() {
+    if(this.props.type == 'MAMP'){
+      this.log("Begin Installing Wordpress...");
+      this.spawnCommandSync('wp', ['core', 'download', '--skip-content']);
+      this.spawnCommandSync('wp', ['config', 'create', '--dbname='+this.props.dbname, '--dbuser='+this.props.dbuser, '--dbpass='+this.props.dbpass]);
+      this.spawnCommandSync('wp', ['db', 'create']);
+      if(this.props.autogen_pass){
+        this.spawnCommandSync('wp', ['core', 'install', '--url=http://'+this.props.url, '--title='+this.props.client_name, '--admin_user='+this.props.admin_user, '--admin_email='+this.props.admin_email, '--skip-email']);
+      }else{
+        this.spawnCommandSync('wp', ['core', 'install', '--url=http://'+this.props.url, '--title='+this.props.client_name, '--admin_user='+this.props.admin_user, '--admin_email='+this.props.admin_email, '--admin_password='+this.props.admin_password, '--skip-email']);
+      }
+      this.spawnCommandSync('wp', ['theme', 'activate', this.props.theme_key]);
+      this.spawnCommandSync('wp', ['plugin', 'activate', '--all']);
+      this.spawnCommandSync('wp', ['plugin', 'deactivate', 'BlenndPreLoader']);
+      this.spawnCommandSync('wp', ['post', 'create', '--post_status=publish', '--post_title=Homepage', '--post_type=page']);
+      this.spawnCommandSync('wp', ['post', 'create', '--post_status=publish', '--post_title=About', '--post_type=page']);
+      this.spawnCommandSync('wp', ['post', 'create', '--post_status=publish', '--post_title=Contact', '--post_type=page']);
+      this.spawnCommandSync('wp', ['post', 'create', '--post_status=publish', '--post_title=Style Guide', '--post_type=page']);
+      this.spawnCommandSync('wp', ['post', 'create', '--post_status=publish', '--post_title=Style Forms', '--post_type=page']);
+      this.spawnCommandSync('wp', ['option', 'update', 'blogdescription', this.props.blogdescription]);
+      this.spawnCommandSync('wp', ['option', 'update', 'blog_public', 0]);
+      this.spawnCommandSync('wp', ['option', 'update', 'show_on_front', 'page']);
+      this.spawnCommandSync('wp', ['option', 'update', 'page_on_front', 4]); //Find a way to get the homepage ID
+      this.spawnCommandSync('wp', ['rewrite', 'structure', '/%postname%/']);
+      // this.spawnCommandSync('wp', ['rewrite', 'flush']); // Need mod_rewrite in config.yaml
+    }else {
+      this.log("Begin Installing Wordpress...");
+      this.spawnCommandSync('wp', ['core', 'download', '--skip-content']);
+    }
+
+    this.log("Compiling Theme Files...");
+    this.spawnCommandSync('npm', ['run', 'dev']);
+
+    // Configure git
+    this.log("Initializing GIT Repositor");
+    this.spawnCommandSync('git', ['init']);
+    this.spawnCommandSync('git', ['add', '.']);
+    this.spawnCommandSync('git', ['commit', '-m', 'Initial Commit']);
+    this.spawnCommandSync('git', ['checkout', '-b', 'staging']);
+    this.spawnCommandSync('git', ['checkout', '-b', 'dev']);
+
+    this.log("Opening Site...");
+    this.spawnCommandSync('open', ['http://'+this.props.url]);
+    // this.spawnCommandSync('npm', ['run', 'watch']);
+  }
+
 };
---
+++ generators/app/templates/webpack.mix.js
@@ -0,0 +1,40 @@
+let mix = require('laravel-mix');
+let path = 'wp-content/themes/<%= theme %>/';
+
+mix.webpackConfig({
+    module: {
+        rules: [
+            {
+                test: /\.scss/,
+                loader: 'import-glob-loader'
+            },
+            {
+                test: /\.js/,
+                loader: 'import-glob-loader'
+            }
+        ]
+        }
+    })
+   .js(path + 'assets/js/scripts.js', path + 'dist/js')
+   .sass(path + 'assets/scss/style.scss', path + 'dist/css', {
+        implementation: require('node-sass')
+    })
+    .options({
+        processCssUrls: false,
+        autoprefixer: {
+            options: {
+                browsers: [
+                    'defaults',
+                ]
+            }
+        }
+    })
+   .setPublicPath(path + 'dist')
+   .browserSync({
+        proxy: '<%= url %>',
+        files: [
+            path + 'dist/css/{*,**/*}.css',
+            path + 'dist/js/{*,**/*}.js',
+            path + '**/*.php'
+        ]
+    });
