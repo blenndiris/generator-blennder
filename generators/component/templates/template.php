2f2a61c3016f67db84a346b3594c1cb0df40850d
Seth Whitaker <sdw3489@gmail.com>
2019-06-06 10:53:42 -0600

    Add component_header & anchor jump link & clean up css

--- generators/component/templates/template.php
+++ generators/component/templates/template.php
@@ -1,8 +1,18 @@
 <?php if ( can_render_component( $fields ) ): ?>
 
 <section <?php echo component_attributes( $fields ); ?> >
+
+	<?php if(!empty($fields['settings_anchor_selector'])): ?>
+	<a id="<?php echo $fields['settings_anchor_selector'];?>" class="anchor"></a>
+	<?php endif; ?>
+
 	<div class="container">
+
+		<?php component_header( $fields ); ?>
+
+		<!-- Component Template -->
 		<h1><%= label %></h1>
+
 	</div><!-- /.container -->
 </section><!-- /#section -->
 
